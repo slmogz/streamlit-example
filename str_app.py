788257bfb14542a6810b1e8a6b9e33b373712c8c
slmogz <150812271+slmogz@users.noreply.github.com>
2024-01-22 14:59:33 +0300

    Update str_app.py

--- str_app.py
+++ str_app.py
@@ -1,13 +1,24 @@
 
 import streamlit
-python -m pip install snowflake-connector-python
+import snowflake.connector
+import pandas
+import requests
+from urllib.error 
+import URL.Error
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list=my_fruit_list.setindex('Fruit')
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
+
 
 streamlit.title('My parents new healthy dinner')
 streamlit.header('🍕Breakfast Menu')
 streamlit.text('🍓Omega 3 & Blueberry Oatmeal')
 streamlit.text('🍸Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚Hard-Boiled Free-Range Egg')
-import snowflake.connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
