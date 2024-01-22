06b0c6f385cca808ac2af2b9b79c7a9c0d6f230c
slmogz <150812271+slmogz@users.noreply.github.com>
2024-01-22 13:55:02 +0300

    Create str_app.py

---
+++ str_app.py
@@ -0,0 +1,14 @@
+
+import streamlit
+streamlit.title('My parents new healthy dinner')
+streamlit.header('🍕Breakfast Menu')
+streamlit.text('🍓Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍸Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚Hard-Boiled Free-Range Egg')
+import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
